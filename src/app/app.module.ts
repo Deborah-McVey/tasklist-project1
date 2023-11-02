983e213f17094aacc1276152f2bbcd6432bf56a2
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-11-02 13:06:32 -0400

    taskit project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
+import { KanbanComponent } from './kanban/kanban.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TasklistComponent,
+    KanbanComponent
   ],
   imports: [
     BrowserModule
