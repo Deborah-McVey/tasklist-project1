5147cf8caa2367928e9a1079f72b371f985f93bc
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-11-04 16:15:43 -0400

    working on datepicker

--- src/app/dialog-overview-example-dialog/dialog-overview-example-dialog.component.ts
+++ src/app/dialog-overview-example-dialog/dialog-overview-example-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { DialogData } from '../dialog-overview-example/dialog-overview-example.component';
+/* import * as _moment from 'moment'; */
 
 @Component({
   selector: 'dialog-overview-example-dialog',
@@ -15,4 +16,18 @@ export class DialogOverviewExampleDialog {
   onNoClick(): void {
     this.dialogRef.close();
   }
+
+/*   const moment = _moment;
+
+  releaseDate = new FormControl(moment("10-20-2020", "MM-DD-YYYY"));
+
+  setDefaultDate() {
+    this.studentForm.patchValue({
+      dateOfBirth: moment("12/25/1995", "MM/DD/YYYY"),
+      admDateRange: {
+      startDate: moment("June 01, 2020", "MMM DD,YYYY"),
+      endDate: moment("July 31, 2020", "MMM DD,YYYY")
+      }
+    });
+   }  */
 }
